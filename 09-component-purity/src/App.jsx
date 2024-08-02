6c7dd0d7ac7aa235975f074b76a6ce1bf10e5cc6
kimHyeonJu <kimhyesom1202@gmail.com>
2024-08-02 17:39:44 +0900

    [준비] 학습자료준비
    - 09-component-purity

---
+++ 09-component-purity/src/App.jsx
@@ -0,0 +1,11 @@
+import AvatarListPage from './pages/AvatarListPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AvatarListPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ 09-component-purity/src/main.jsx
@@ -0,0 +1,15 @@
+import '@/styles/main.css';
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// > components
+import App from './App';
+
+const container = document.getElementById('react-app');
+
+if (!container) throw new Error('문서에 "#app" 요소가 존재하지 않습니다.');
+
+createRoot(container).render(
+  <>
+    <App />
+  </>
+);
