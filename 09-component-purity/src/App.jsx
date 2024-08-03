473570472f2719e8ed30126a90de9df39e4a7307
kimHyeonJu <kimhyesom1202@gmail.com>
2024-08-03 17:38:46 +0900

    [학습] 리액트 컴포넌트의 순수성과 StrictMode

--- 09-component-purity/src/App.jsx
+++ 09-component-purity/src/App.jsx
@@ -2,6 +2,16 @@ import AvatarListPage from '@/pages/AvatarListPage';
 // > data
 import avatarsData from './data/avatars';
 
+// 리액트 컴포넌트(함수)는 순수 해야 한다.
+/*
+  ※ 순수한지 어떻게 확인해야할까?
+  - 2번 실행(출력이 같으면 순수) 
+  - StrictMode로 개발상에서 발생할 수 있는 일반적인 오류를 찾을 때 사용
+  
+  ※ StrictMode는 ①실행mount → unmount → ②실행mount
+  - 개발 중일 때만 2번 실행해서 일반적인 문제를 진단
+  - 배포 되었을 때는 1번만 실행
+*/
 function App() {
   return (
     <div className="App">
--- 09-component-purity/src/main.jsx
+++ 09-component-purity/src/main.jsx
@@ -1,5 +1,5 @@
 import '@/styles/main.css';
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './learn/about-purity';
 import './learn/about-side-effects';
@@ -11,7 +11,7 @@ const container = document.getElementById('react-app');
 if (!container) throw new Error('문서에 "#app" 요소가 존재하지 않습니다.');
 
 createRoot(container).render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
