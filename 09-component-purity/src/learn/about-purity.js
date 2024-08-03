38b1a28138948842b011d7ea2931805e7991a2e7
kimHyeonJu <kimhyesom1202@gmail.com>
2024-08-03 16:30:59 +0900

    [학습] 순수 함수
    - 동일 입력 → 동일 출력

--- 09-component-purity/src/learn/about-purity.js
+++ 09-component-purity/src/learn/about-purity.js
@@ -1,14 +1,14 @@
 // --------------------------------------------------------------------------
 // ✅ 순수 함수란?
 // --------------------------------------------------------------------------
-// * - [ ] 함수의 순수성(purity)이란?
-// * - [ ] 함수가 순수한 지 여부는 어떻게 확인해야 할까?
+// - [x] 함수의 순수성(purity)이란?
+// - [x] 함수가 순수한 지 여부는 어떻게 확인해야 할까?
 // --------------------------------------------------------------------------
 
-/* * @types((text: string, limit?:number) => string)*/
+/**@types{(text: string, limit?:number) => string}*/
 export function truncateText(text, limit = 100) {
   if (text.length > limit) {
-    return text.slide(0, limit) + '...';
+    return text.slice(0, limit) + '...';
   }
 
   return text;
@@ -17,5 +17,6 @@ export function truncateText(text, limit = 100) {
 // * 이 함수는 동일한 입력을 받았을 때, 동일한 결과를 반환한다. (순수함이 보장된다.)
 // * 순수 함수의 요건 : 동일 입력 → 동일 출력
 // * 순수함이란? 계산된 결과가 동일함을 말한다.
-const testText = 'hyeonju'.repeat(5);
+const testText = 'hyeonju'.repeat(20);
+console.log(testText.length);
 console.log(truncateText(testText));
--- 09-component-purity/src/main.jsx
+++ 09-component-purity/src/main.jsx
@@ -1,6 +1,7 @@
 import '@/styles/main.css';
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './learn/about-purity';
 // > components
 import App from './App';
 
